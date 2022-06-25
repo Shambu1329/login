5b4557bbdf2baf7641f02c63d6ce98325a4a2ba8
Shambu1329 <shambukrishna34@gmail.com>
2022-06-25 15:27:06 +0530

    completed

---
+++ login.js
@@ -0,0 +1,60 @@
+loginBtnevent=()=>{
+    Username=(document.getElementById("Username").value)
+    Password=(document.getElementById("Password").value)
+
+    console.log(Username)
+    alert(Username)
+
+    console.log(Password)
+    alert(Password)
+}
+
+
+
+
+registerBtnevent=()=>{
+    Name=(document.getElementById("Name").value)
+    Address=(document.getElementById("Address").value)
+    MobileNo=(document.getElementById("MobileNo").value)
+    EmailId=(document.getElementById("EmailId").value)
+    BloodGroup=(document.getElementById("BloodGroup").value)
+    College=(document.getElementById("College").value)
+    UserName=(document.getElementById("UserName").value)
+    Password=(document.getElementById("Password").value)
+    ConfirmPassword=(document.getElementById("ConfirmPassword").value)
+
+
+    console.log(Name)
+    alert(Name)
+
+    console.log(Address)
+    alert(Address)
+
+    console.log(MobileNo)
+    alert(MobileNo)
+
+    console.log(EmailId)
+    alert(EmailId)
+
+    console.log(BloodGroup)
+    alert(BloodGroup)
+
+    console.log(College)
+    alert(College)
+
+    console.log(UserName)
+    alert(UserName)
+
+    console.log(Password)
+    alert(Password)
+
+    console.log(ConfirmPassword)
+    alert(ConfirmPassword)
+
+
+    
+
+    
+
+}
+
